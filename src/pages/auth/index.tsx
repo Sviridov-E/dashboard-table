ef3331f2002ad183cfaa70d09ca8af5ec6ac24d2
Sviridov-E <sviridoveg1996@gmail.com>
2026-03-16 13:02:53 +0300

    feat: inputs on auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -4,6 +4,9 @@ import { Button } from '@/shared/ui'
 import { Checkbox } from '@/shared/ui/checkbox'
 import { Field } from '@/shared/ui/field'
 import { Label } from '@/shared/ui/label'
+import { Spinner } from '@/shared/ui/spinner'
+import { Lock, LockKeyhole, UnlockKeyhole, User, X } from 'lucide-react'
+import { useState } from 'react'
 import { Controller, useForm, type SubmitHandler } from 'react-hook-form'
 import { Navigate, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
@@ -23,27 +26,35 @@ export const AuthPage = () => {
     handleSubmit,
     control,
     formState: { errors },
+    resetField,
   } = useForm<AuthFormValues>()
 
   const { user, idleStatus } = useUserStore()
 
   const navigate = useNavigate()
 
+  const [pending, setPending] = useState(false)
+
   const onSubmit: SubmitHandler<AuthFormValues> = async ({
     username,
     password,
     remember,
   }) => {
+    setPending(true)
     try {
       await login({ username, password, remember })
       navigate('/', { replace: true })
+      setPending(false)
     } catch (e) {
+      setPending(false)
       toast.error('Ошибка', {
         description:
           e instanceof Error ? e.message : 'При аутентификации возникла ошибка',
       })
     }
   }
+
+  const [showPass, setShowPass] = useState(false)
 
   if (user && idleStatus === 'done') return <Navigate to='/' />
 
@@ -53,11 +64,11 @@ export const AuthPage = () => {
         <div className='p-px rounded-[calc(var(--radius-4xl)-6px)] bg-linear-to-b from-20% from-neutral-200 to-transparent'>
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className='bg-card p-12 flex flex-col items-center rounded-[calc(var(--radius-4xl)-7px)] bg-linear-to-b to-50% from-[#23232308] to-transparent'
+            className='bg-card md:px-12 px-6 py-12 flex flex-col items-center rounded-[calc(var(--radius-4xl)-7px)] bg-linear-to-b to-50% from-[#23232308] to-transparent'
           >
             <Logo className='mb-8' />
 
-            <h1 className='text-center text-[40px]/[110%] font-semibold tracking-[-0.015em] text-gray-800'>
+            <h1 className='text-center text-3xl md:text-[40px]/[110%] font-semibold tracking-[-0.015em] text-gray-800'>
               Добро пожаловать!
             </h1>
 
@@ -75,6 +86,20 @@ export const AuthPage = () => {
                 error={errors.username ? errors.username.message : null}
                 label='Логин'
                 className='mt-8'
+                Icon={User}
+                actionButton={
+                  <Button
+                    type='button'
+                    variant='ghost'
+                    size='sm'
+                    className='absolute right-1 top-1/2 h-12 w-12 -translate-y-1/2 active:-translate-y-1/2 p-0 hover:bg-transparent hover:text-gray-500 focus:border-none focus:ring-0! text-gray-300'
+                    onClick={() => {
+                      resetField('username')
+                    }}
+                  >
+                    <X className='size-6' />
+                  </Button>
+                }
               />
               <SignInInput
                 {...register('password', {
@@ -84,8 +109,26 @@ export const AuthPage = () => {
                 })}
                 error={errors.password ? errors.password.message : null}
                 label='Пароль'
-                type='password'
+                type={showPass ? 'text' : 'password'}
                 className='mt-4'
+                Icon={Lock}
+                actionButton={
+                  <Button
+                    type='button'
+                    variant='ghost'
+                    size='sm'
+                    className='absolute right-1 top-1/2 h-12 w-12 -translate-y-1/2 active:-translate-y-1/2 p-0 hover:bg-transparent hover:text-gray-500 focus:border-none focus:ring-0! text-gray-300'
+                    onClick={() => {
+                      setShowPass(state => !state)
+                    }}
+                  >
+                    {showPass ? (
+                      <UnlockKeyhole className='size-6' />
+                    ) : (
+                      <LockKeyhole className='size-6' />
+                    )}
+                  </Button>
+                }
               />
             </div>
 
@@ -103,10 +146,32 @@ export const AuthPage = () => {
                   )
                 }}
               />
-              <Label htmlFor='remember-checkbox'>Запомнить данные</Label>
+              <Label
+                htmlFor='remember-checkbox'
+                className='text-base text-gray-400'
+              >
+                Запомнить данные
+              </Label>
             </Field>
 
-            <Button className='mt-5 w-full'>Войти</Button>
+            <Button className='mt-5 w-full relative' disabled={pending}>
+              {pending ? <Spinner className='size-7' /> : 'Войти'}
+            </Button>
+
+            <div className='mt-4 w-full flex items-center gap-2.5'>
+              <hr className='grow border-gray-300' />
+              <span className='shrink-0 text-base text-gray-300'>или</span>
+              <hr className='grow border-gray-300' />
+            </div>
+
+            <div className='mt-8'>
+              <span className='text-lg text-gray-500'>
+                Нет аккаунта?
+                <Button variant='link' className=' h-auto underline'>
+                  Создать
+                </Button>
+              </span>
+            </div>
           </form>
         </div>
       </div>
--- src/pages/auth/ui/sign-in-input.tsx
+++ src/pages/auth/ui/sign-in-input.tsx
@@ -1,19 +1,25 @@
 import { Field, FieldDescription, FieldLabel } from '@/shared/ui/field'
 import { Input } from '@/shared/ui/input'
 
-import { User } from 'lucide-react'
-import { useId, type HTMLProps } from 'react'
+import React, { useId, type HTMLProps } from 'react'
 
 export const SignInInput = ({
   label,
   className,
   error,
+  Icon,
+  actionButton,
   ...inputProps
 }: {
   label: string
   placeholder?: string
   className?: string
   error?: string | null
+  Icon: React.ForwardRefExoticComponent<
+    Omit<{ className?: string; size?: number }, 'ref'> &
+      React.RefAttributes<SVGSVGElement>
+  >
+  actionButton?: React.ReactNode
 } & HTMLProps<HTMLInputElement>) => {
   const id = useId()
   return (
@@ -22,7 +28,7 @@ export const SignInInput = ({
         {label}
       </FieldLabel>
       <div className='relative'>
-        <User
+        <Icon
           className='absolute top-1/2 left-3 -translate-y-1/2 text-muted-foreground'
           size={24}
         />
@@ -32,6 +38,7 @@ export const SignInInput = ({
           aria-invalid={!!error}
           id={id}
         />
+        {actionButton}
       </div>
       {!!error && <FieldDescription>{error}</FieldDescription>}
     </Field>
